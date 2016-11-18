212652f4a34ca2e79d80262730752b39560fe99f
lindan <jackcoor@hotmail.com>
2016-11-18 11:19:52 +0800

    fix some bugs

--- okhttputil/src/main/java/com/lance/network/okhttputil/callback/JsonCallback.java
+++ okhttputil/src/main/java/com/lance/network/okhttputil/callback/JsonCallback.java
@@ -3,21 +3,31 @@ package com.lance.network.okhttputil.callback;
 import com.lance.network.okhttputil.utils.JSONUtil;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 
 import okhttp3.Response;
 
 public abstract class JsonCallback<T> extends Callback<T> {
     private Class<T> clazz;
+    private Type type;
 
     public JsonCallback(Class<T> clazz) {
         this.clazz = clazz;
     }
 
+    public JsonCallback(Type type) {
+        this.type = type;
+    }
+
     @Override
     public T parseNetworkResponse(Response response, int id) throws IOException {
-        if(response != null && response.body() != null) {
+        if (response != null && response.body() != null) {
             String json = response.body().string();
-            return JSONUtil.getObjectFromJson(json, clazz);
+            if (clazz != null) {
+                return JSONUtil.getObjectFromJson(json, clazz);
+            } else if (type != null) {
+                return JSONUtil.getObjectFromJson(json, type);
+            }
         }
         return null;
     }
